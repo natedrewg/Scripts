c22a5ef360ecee825300ed3b6e7e8d81d2234f85
Nathan Gilbert <natedrewg@gmail.com>
2022-11-01 12:56:50 -0400

    Using function to simplify work

--- sugarcane.py
+++ sugarcane.py
@@ -2,51 +2,32 @@ import keyboard
 import time
 import pyautogui
 
-i = 0
 
-keyboard.wait('o')
-
-while i < 30:
+def once(i):
   pyautogui.mouseDown(button='left')
   keyboard.press('S')
 
-
-  for j in range(21):
-    time.sleep(1)
-    if keyboard.read_key('k'):
-      keyboard.release('S')
-      pyautogui.mouseUp(button='left')
-      keyboard.wait('k')
-      keyboard.press('S')
-      pyautogui.mouseDown(button='left')
-      j = j + 1
-    elif keyboard.read_key('o'):
-      keyboard.release('S')
-      pyautogui.mouseUp(button='left')
-      quit()
-    else:
-      j = j + 1
-
-
+  time.sleep(21)
+  
   keyboard.release('S')
   keyboard.press('A')
 
+  time.sleep(21)
 
-  for k in range(21):
-    time.sleep(1)
-    if keyboard.read_key('k'):
-      keyboard.release('S')
-      pyautogui.mouseUp(button='left')
-      keyboard.wait('K')
-      keyboard.press('A')
-      pyautogui.mouseDown(button='left')
-    elif keyboard.read_key('o'):
-      keyboard.release('S')
-      pyautogui.mouseUp(button='left')
-      quit()
-    else:
-      k = k + 1
-
-      
   keyboard.release('A')
   i = i + 1
+
+
+def main():
+
+  i = 0
+
+  keyboard.wait('o')
+
+  while i < 30:
+  
+    once(i)
+      
+
+  print("You went through " + i + "iterations!")
+main()
