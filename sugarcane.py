1e8cf207f6b727ca2cccd5c45112c4adeb189ce8
Nathan Gilbert <105817717+natedrewg@users.noreply.github.com>
2022-11-02 10:20:46 -0400

    Update sugarcane.py

--- sugarcane.py
+++ sugarcane.py
@@ -2,7 +2,7 @@ import keyboard
 import time
 import pyautogui
 
-
+#Run through one round of the sugarcane farm
 def once(i):
   pyautogui.mouseDown(button='left')
   keyboard.press('S')
@@ -16,7 +16,9 @@ def once(i):
 
   keyboard.release('A')
 
+#Check if the user is pressing a button to stop the movement of the character
 def check(direction):
+ #Pressing K will stop the character until you press k again, then the character will start moving again from where it left off
       if keyboard.is_pressed('k'):
         keyboard.release('S')
         keyboard.release('A')
@@ -24,18 +26,20 @@ def check(direction):
         keyboard.wait('k')
         keyboard.press(direction)
         pyautogui.mouseDown(button='left')
+ #Pressing o will stop the character all together and kill the program
       if keyboard.is_pressed('o'):
         keyboard.release('S')
         keyboard.release('A')
         pyautogui.mouseUp(button='left')
         quit()
 
+#This will fully stop all processes on the character
 def stop():
   keyboard.release('S')
   keyboard.release('A')
   pyautogui.mouseUp(button='left')
 
-
+#This function acts as a timer that will allow the character to know when to switch directions
 def sleep(direction):
   for i in range(20):
     time.sleep(1)
@@ -44,7 +48,6 @@ def sleep(direction):
 def main():
 
   i = 0
-
   keyboard.wait('o')
 
   while i < 5:
@@ -52,6 +55,6 @@ def main():
     i = i + 1
       
   stop()
-
   print("You went through " + str(i) + " iterations!")
+  
 main()
